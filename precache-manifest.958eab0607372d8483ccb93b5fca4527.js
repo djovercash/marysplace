2709dd9af3729e4026502b810fe6c3e9abff29da
Overcash <dovercash@LT-67058.local>
2018-12-02 11:52:41 -0500

    Updates

---
+++ precache-manifest.958eab0607372d8483ccb93b5fca4527.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f51c6b7c44b64c695aef",
+    "url": "/marysplace/static/js/runtime~main.f51c6b7c.js"
+  },
+  {
+    "revision": "efd64ead63d9e657021e",
+    "url": "/marysplace/static/js/main.efd64ead.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/marysplace/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "efd64ead63d9e657021e",
+    "url": "/marysplace/static/css/main.a434a9b1.chunk.css"
+  },
+  {
+    "revision": "89a1608c7dbc51000d108092ebb8311d",
+    "url": "/marysplace/index.html"
+  }
+];
